4ca4262e7aa51981c752246ec2fed067f396b781
devsh <digimon.1740@gmail.com>
2019-09-16 00:53:35 +0900

    router 테스트 추가

--- src/test/kotlin/com/digimon/demo/TodoApplicationTests.kt
+++ src/test/kotlin/com/digimon/demo/TodoApplicationTests.kt
@@ -1,29 +1,62 @@
 package com.digimon.demo
 
 import com.digimon.demo.domain.todo.Todo
+import com.digimon.demo.domain.todo.TodoRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
-@WebFluxTest
 @RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoApplicationTests {
 
     @Autowired
     lateinit var webClient: WebTestClient
 
+    @MockBean
+    lateinit var repo: TodoRepository
+
+    lateinit var todo: Todo
+
+    @Before
+    fun setUp() {
+        todo = Todo(id = 1L, content = "I have to finish my work by tomorrow")
+    }
+
     @Test
     @Throws(Exception::class)
     fun `test should return a list of todo`() {
+        given(repo.findAll()).willReturn(Flux.just(todo))
+
         webClient.get().uri("/todos").accept(MediaType.APPLICATION_JSON)
             .exchange()
             .expectStatus().isOk
             .expectBodyList(Todo::class.java)
-            .hasSize(3)
+            .hasSize(1)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `test should return a todo by id`() {
+        given(repo.findById(1L)).willReturn(Mono.just(todo))
+
+        val responseBody: Todo? = webClient.get().uri("/todos/{id}", 1L).accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(Todo::class.java)
+            .returnResult().responseBody
+
+        assertThat(responseBody?.content).isEqualTo("I have to finish my work by tomorrow")
     }
 
 }
